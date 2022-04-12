b17681e4951910cee64e0f139200f59a71b4d98d
esdras <esdrassousa76@gmail.com>
2022-04-12 01:03:54 -0300

    'portifole_01'

--- portifole/resources/js/app.js
+++ portifole/resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import { createApp } from "vue";
+import App from './components/App.vue'
+import Router from './components/Router/Router.js'
+
+createApp(App)
+    .use(Router)
+    .mount('#app')
